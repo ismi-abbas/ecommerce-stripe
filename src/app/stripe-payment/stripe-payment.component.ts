8b0f804665b7e506d80d75eeef898db4f29dbd7c
Abbas <muhdabbas98@gmail.com>
2023-05-15 12:25:49 +0800

    stripe component

--- src/app/stripe-payment/stripe-payment.component.ts
+++ src/app/stripe-payment/stripe-payment.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stripe-payment',
   templateUrl: './stripe-payment.component.html',
-  styleUrls: ['./stripe-payment.component.css']
+  styleUrls: ['./stripe-payment.component.css'],
 })
-export class StripePaymentComponent {
-
-}
+export class StripePaymentComponent {}
